733db5a43328d48bbfe732968266dcd44e365950
fraykate <34926259+fraykate@users.noreply.github.com>
2022-06-06 11:20:50 -0600

    Update and rename name_your_polygon to name_your_polygon.py
    updated for clarity

--- name_your_polygon.py
+++ name_your_polygon.py
@@ -1,25 +1,26 @@
 import turtle
 
-response = ("5 30 green red")
+response = input("Hey stranger! Can you please tell me the number of sides, length of each side, line color and fill color for a polygon of your choice?")
 response_list = response.split()
-#inputs = input("Hey stranger! Can you please tell me the number of sides, length of each side, line color and fill color for a polygon?")
 
+# assign your reponses to variables
 sides = int(response_list[0])
 length = int(response_list[1])
 linecolor = response_list[2]
 fillcolor = response_list[3]
 
+# compute the interior angle of the polygon
 angle = 180 - (((180*sides)-360) / sides)
 
-print(sides, length, linecolor, fillcolor, angle)
+# build your screen and turtle
 screen = turtle.Screen()
 polly = turtle.Turtle()
-
 polly.shape()
 polly.color(linecolor)
 polly.fillcolor = (fillcolor)
 polly.begin_fill()
 
+# iterate over the number of sides
 for side in range(sides):
     polly.forward(length)
     polly.left(angle)
